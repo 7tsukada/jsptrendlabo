d56e02d999009cc0b0177ad068bbedcb4d081daa
kyoshic9 <yoshino.ko@jsp-com.co.jp>
2016-06-18 06:27:27 +0000

    登録と読込を追加

--- routes/kyoshi/message.js
+++ routes/kyoshi/message.js
@@ -2,21 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/message', function(req, res) {
-    var messages = [
-            {
-                message: "message1",
-                post: "1999-1-1 14:30"
-            },
-            {
-                message: "message2",
-                post: "2009-12-13 5:20"
-            },
-        ];
-    res.render('kyoshi/message', { "messages": messages });
+    res.redirect(req.baseUrl + '/message/list');
 });
 
-router.get('/message/post', function(req, res) {
-    res.redirect('../message');
+router.get('/message/list', function(req, res) {
+    var KyoshiMessage = global.db.KyoshiMessage;
+    KyoshiMessage.findAll({order: 'post DESC'})
+    .error(function(err){
+        res.send(err);
+    })
+    .then(function(messages){
+        res.render('kyoshi/message', { messages: messages });
+    });
+});
+
+router.post('/message/post', function(req, res) {
+    var KyoshiMessage = global.db.KyoshiMessage;
+    var message = req.body.message;
+    var posted = new Date();
+    KyoshiMessage.create({
+        message: message, 
+        post: posted
+        
+    })
+    .error(function(err){
+        res.send(err);
+    })
+    .then(function(result){
+        res.redirect(req.baseUrl + '/message/list');
+    });
 });
 
 module.exports = router;
